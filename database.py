fcbfe38f2c737004c20de8b44ffe765719514d00
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-11-15 20:58:00 +0530

    misc

--- database.py
+++ database.py
@@ -1,34 +1,41 @@
 import sqlite3
 
 if __name__ == "__main__":
-    conn = sqlite3.connect("example.db")
+    conn = sqlite3.connect("main.db")
     c = conn.cursor()
     c.execute("PRAGMA foreign_keys = ON")
     c.execute(
         """
         CREATE TABLE user
-        (id integer primary key autoincrement not null,
-        email text not null unique, password TEXT)
+        (
+        id integer primary key autoincrement not null,
+        email text not null unique, password TEXT
+        )
         """
     )
     c.execute(
         """
         CREATE TABLE activity_options
-        (id integer primary key autoincrement not null,
+        (
+        id integer primary key autoincrement not null,
         user_id integer not null,
         activity text not null,
         foreign key (user_id) references user(id),
-        constraint unq unique (user_id, activity))
+        constraint unq unique (user_id, activity)
+        )
         """
     )
     c.execute(
         """
         CREATE TABLE activities
-        (duration int not null,
+        (
+        duration int not null,
         date datetime not null,
         activity_options_id,
         foreign key(activity_options_id) references
-            activity_options(id))"""
+            activity_options(id)
+        )
+        """
     )
     conn.commit()
     conn.close()
