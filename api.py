e547e95bbf996f92b2615aa12b4f4a1596aa975f
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-11-15 22:16:37 +0530

    basic api

---
+++ api.py
@@ -0,0 +1,72 @@
+from bottle import run, request, ServerAdapter, Bottle
+from bottle.ext import sqlite
+from beaker.middleware import SessionMiddleware
+from cheroot import wsgi
+from cheroot.ssl.builtin import BuiltinSSLAdapter
+import ssl
+
+app = Bottle()
+
+
+@app.route("/whoami", method="GET")
+def whoami():
+    try:
+        username = current_user()
+    except Exception:
+        username = None
+    return {"username": username}
+
+
+@app.route("/login", method="POST")
+def login():
+    name = request.forms.UserName
+    passwd = request.forms.Password
+    if passwd == "123":
+        session = beaker_session()
+        session["username"] = name
+
+
+@app.route("/logout", method="POST")
+def logout():
+    session = beaker_session()
+    session.delete()
+
+
+def beaker_session():
+    return request.environ.get("beaker.session")
+
+
+def current_user():
+    session = beaker_session()
+    username = session.get("username", None)
+    if username is None:
+        raise Exception("Unauthenticated user")
+    return username
+
+
+class SSLCherryPyServer(ServerAdapter):
+    def run(self, handler):
+        server = wsgi.Server((self.host, self.port), handler)
+        server.ssl_adapter = BuiltinSSLAdapter("cacert.pem", "privkey.pem")
+        server.ssl_adapter.context.options |= ssl.OP_NO_TLSv1
+        server.ssl_adapter.context.options |= ssl.OP_NO_TLSv1_1
+        try:
+            server.start()
+        finally:
+            server.stop()
+
+
+session_opts = {
+    "session.type": "file",
+    "session.data_dir": "./data",
+    "session.auto": True,
+    "save_accessed_time": True,
+    "secure": True,
+    "timeout": 300,
+}
+
+if __name__ == "__main__":
+    plugin = sqlite.Plugin(dbfile="./main.db")
+    app.install(plugin)
+    session_app = SessionMiddleware(app, session_opts)
+    run(app=session_app, host="0.0.0.0", port=443, server=SSLCherryPyServer, debug=True)
