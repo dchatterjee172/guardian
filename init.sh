21327e0eec01b9a80add615fdf01817f9d65544c
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-11-20 21:53:12 +0530

    fixes

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
+cp guadian.ngnix.conf /etc/nginx/conf.d/
+nginx -g 'pid /run/nginx/nginx.pid;'
+nginx -s reload
 source $HOME/.poetry/env
 poetry install
 poetry shell
-guadian.ngnix.conf /etc/nginx/conf.d
-ngnix -s reload
